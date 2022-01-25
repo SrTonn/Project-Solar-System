28bf2bccabb94ba2af5d10354226c8a0376515ff
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-25 19:01:21 -0300

    SolarSystem estilizado

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,3 +1,4 @@
+import '../styles/SolarSystem.css';
 import React, { Component } from 'react';
 import Title from './Title';
 import Planets from '../data/planets';
@@ -6,16 +7,18 @@ import PlanetCard from './PlanetCard';
 export default class SolarSystem extends Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <>
         <Title headline="Planetas" />
-        {Planets.map(({ name, image }) => (
-          <PlanetCard
-            key={ name }
-            planetName={ name }
-            planetImage={ image }
-          />
-        ))}
-      </div>
+        <div data-testid="solar-system" className="solar-system-container">
+          {Planets.map(({ name, image }) => (
+            <PlanetCard
+              key={ name }
+              planetName={ name }
+              planetImage={ image }
+            />
+          ))}
+        </div>
+      </>
     );
   }
 }
