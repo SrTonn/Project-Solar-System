e6608a9500ee4580e8e6555511a2d77e938229f7
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-25 18:01:23 -0300

    definido um className para cada planeta

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,6 +6,16 @@ export default class PlanetCard extends Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
+        <img
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+          className={
+            planetName
+              .toLowerCase()
+              .normalize('NFD')
+              .replace(/[\u0300-\u036f]/g, '')
+          }
+        />
         <p data-testid="planet-name">
           {planetName}
         </p>
