3079eae24c8392fa3854789fc0e06cf23228b436
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-25 22:02:53 -0300

    Missions estilizado

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,3 +1,4 @@
+import '../styles/Missions.css';
 import React, { Component } from 'react';
 import MissionCard from './MissionCard';
 import Title from './Title';
@@ -6,17 +7,19 @@ import missions from '../data/missions';
 export default class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">
+      <>
         <Title headline="Missões" />
-        {missions.map(({ name, year, country, destination }) => (
-          <MissionCard
-            key={ name }
-            name={ name }
-            year={ year }
-            country={ country }
-            destination={ destination }
-          />))}
-      </div>
+        <div data-testid="missions" className="missions-container">
+          {missions.map(({ name, year, country, destination }) => (
+            <MissionCard
+              key={ name }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />))}
+        </div>
+      </>
     );
   }
 }
